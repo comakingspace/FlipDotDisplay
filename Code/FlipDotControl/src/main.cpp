5d3b305876932094f396f491acb18638775d31e5
NitramLegov <Martin.Vogel@live.com>
2018-12-02 18:11:31 +0100

    setting keepalive

--- Code/FlipDotControl/src/main.cpp
+++ Code/FlipDotControl/src/main.cpp
@@ -288,6 +288,7 @@ void MQTT_connect() {
     MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
     data.MQTTVersion = 3.1;
     data.clientID.cstring = (char *)"FDD_MQTTClient";
+    data.keepAliveInterval = 20;
     rc = MQTTclient.connect(data);
     if (rc != 0)
     {
